4cb17c5a5855aab497e3745f1be1867e92156e4e
Manu Ioan <ioanmanu@yahoo.com>
2022-03-25 15:28:10 +0200

    restart

--- js/script.js
+++ js/script.js
@@ -3,5 +3,34 @@ function onlyOne(checkbox) {
     checkboxes.forEach((item) => {
         if (item !== checkbox) item.checked = false
     })
-    console.log(5);
+    
+}
+
+let cart = document.querySelector('.cart')
+
+const Go =() =>{
+    var checkboxes = document.getElementsByName('check');
+    let ok=0;
+    checkboxes.forEach((item) => {
+        
+        if(item.checked === true)
+        {
+          console.log(item.value);
+          ok=1;
+        }   
+        console.log(ok);
+       
+        
+
+        //console.log(item.name);
+    })
+    if(ok===0)
+    {
+        alert("You did not checked something!")
+    }
+    else{
+        
+    }
+   
+
 }
