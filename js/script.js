f678d63907634e186343ff839c2c295def68c50d
Manu Ioan <ioanmanu@yahoo.com>
2022-03-28 14:05:25 +0300

    s+m

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //     e.preventDefault(); // preventing form from submiting
 // }
 
-
+let x;
 function onlyOne(checkbox) {
     var checkboxes = document.getElementsByName('check')
     
@@ -16,8 +16,16 @@ function onlyOne(checkbox) {
     })
     let cb = checkbox.value
     let sugar = document.querySelector(`.${cb}`)
-    sugar.innerHTML = '<img src="../images/sugar.png" alt=""><div class="much"><p>How much sugar do you want?</p><div class="ps"><button type="button" onclick="plus()">+</button><div class="adg"><div class="patrat"></div></div></div></div>'
-
+    sugar.innerHTML = '<div class="pot" style="margin-bottom:10px;"><img src="../images/sugar.png" alt=""><div class="much"><p>How much sugar do you want?</p><div class="ps"><button type="button" onclick="plus()">+</button><div class="adg"><div class="patrat"></div></div></div></div> </div><div class="pot" ><img src="../images/milk.png" alt=""><div class="much"><p>How much milk do you want?</p><div class="ps"><button type="button" onclick="plusM()">+</button><div class="adgm"><div class="patrat"></div></div></div></div> </div>'
+    
+    x=1;
+    y=1;
+    let sg = document.querySelectorAll('.d');
+    
+    sg.forEach((item)=>{
+        if(item !=sugar)
+        item.innerHTML="";
+    })
 }
 
 let cart = document.querySelector('.cart')
@@ -50,17 +58,55 @@ const Go =() =>{
    
 
 }
-let x=1;
-let adg = document.querySelector('.adg');
+ 
+let y=1;
+
 const plus = ()=>{
     if(x<3)
     x++;
     let qs = ''
     for(let i =0;i<x;i++)
          qs += '<div class="patrat"></div>';
+         let adg = document.querySelector('.adg');
     adg.innerHTML = qs;     
     
 
 
 
+}
+
+
+const minus = () => {
+    if(x>1)
+    x--;
+    let qs = ''
+    for(let i =0;i<x;i++)
+         qs += '<div class="patrat"></div>';
+         let adg = document.querySelector('.adg');
+    adg.innerHTML = qs; 
+
+}
+
+
+const plusM = ()=>{
+    if(y<3)
+    y++;
+    let qs = ''
+    for(let i =0;i<y;i++)
+         qs += '<div class="patrat"></div>';
+         let adgm = document.querySelector('.adgm');
+    adgm.innerHTML = qs;     
+    
+
+
+
+}
+const minusM = ()=>{
+    if(y>1)
+    y--;
+    let qs = ''
+    for(let i =0;i<y;i++)
+         qs += '<div class="patrat"></div>';
+         let adgm = document.querySelector('.adgm');
+    adgm.innerHTML = qs;   
 }
